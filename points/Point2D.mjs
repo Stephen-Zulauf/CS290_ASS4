8366e90cf73db7ae6cf7490b9e9c831055bad2c5
Stephen-Zulauf <szulauf1990@gmail.com>
2025-04-23 12:42:10 -0700

    2d class passing npm test

--- points/Point2D.mjs
+++ points/Point2D.mjs
@@ -1,34 +1,35 @@
-/* 
-* TODO: add your First Name Last Name.
-*/
+/*
+ * Stephen Zulauf zulaufs@osu.edu
+ */
 
 /*
-* Don't change the starter code we have provided in this file.
-* Add your code where we have added TODO comments.
-* If you want, you can remove the TODO comments. Your choice.
-*/
+ * Don't change the starter code we have provided in this file.
+ * Add your code where we have added TODO comments.
+ * If you want, you can remove the TODO comments. Your choice.
+ */
 
 /**
  * Models a point in 2 dimensions
  */
 class Point2D {
-    /**
-     * 
-     * @param {number} x The x-coordinate of the point
-     * @param {number} y The y-coordinate of the point
-     */
-    constructor(x, y) {
-        // TODO: Write your code here
-    }
+  /**
+   *
+   * @param {number} x The x-coordinate of the point
+   * @param {number} y The y-coordinate of the point
+   */
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    /**
-     * 
-     * @param {object} p An instance of the class Point2Dim
-     * @returns The Euclidean distance between this point and p
-     */
-    distanceFrom(p) {
-        // TODO: Write your code here
-    }
+  /**
+   *
+   * @param {object} p An instance of the class Point2Dim
+   * @returns The Euclidean distance between this point and p
+   */
+  distanceFrom(p) {
+    return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
+  }
 }
 
-export {Point2D}
+export { Point2D };
