7c9c6c6f2b1345387dd5470c30aac2208ac5dce6
Stephen-Zulauf <szulauf1990@gmail.com>
2025-04-23 12:56:18 -0700

    3d point class passing npm test

--- points/Point3D.mjs
+++ points/Point3D.mjs
@@ -1,5 +1,32 @@
-/* 
-* TODO: add your First Name Last Name.
-*/
+/*
+ * Stephen Zulauf- zulaufs@wsu.edu
+ */
+import { Point2D } from "./Point2D.mjs";
 
-// TODO: Write your code here
+class Point3D extends Point2D {
+  /**
+   *
+   * @param {number} x The x-coordinate of the point
+   * @param {number} y The y-coordinate of the point
+   * @param {number} z The z-coordinate of the point
+   */
+  constructor(x, y, z) {
+    super(x, y);
+    this.z = z;
+  }
+
+  /**
+   *
+   * @param {object} p An instance of the class Point3Dim
+   * @returns The Euclidean distance between this point and p
+   */
+  distanceFrom(p) {
+    return Math.sqrt(
+      Math.pow(this.x - p.x, 2) +
+        Math.pow(this.y - p.y, 2) +
+        Math.pow(this.z - p.z, 2)
+    );
+  }
+}
+
+export { Point3D };
